8eef325fe4ec5d6f4d0cf3e811172d35732c4209
Risham <rishamsidhu@berkeley.edu>
2020-11-10 13:34:05 -0800

    base occupancy grid structure

---
+++ src/mapping/src/mapping_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+################################################################################
+#
+# Node to wrap the OccupancyGrid2d class.
+#
+################################################################################
+
+from occ_grid import OccupancyGrid2d
+
+import rospy
+import sys
+
+if __name__ == "__main__":
+    rospy.init_node("mapping_node")
+
+    og = OccupancyGrid2d("node/map_points", 50, 50, 0.5, 0.75, "occ_grid/vis")
+    
+    rospy.spin()
